0153bb4c5e617346b43f98d1fdf795bdc628502b
crazyoptimist <crazyoptimist@mail.com>
2021-12-01 05:45:22 -0600

    add order placement

--- sniper/sniper.py
+++ sniper/sniper.py
@@ -3,25 +3,32 @@ import time
 from dotenv import load_dotenv
 from dydx3 import Client
 from dydx3.constants import API_HOST_MAINNET, NETWORK_ID_MAINNET, \
-        ORDER_SIDE_BUY, ORDER_SIDE_SELL, ORDER_TYPE_MARKET, ORDER_STATUS_OPEN, \
+        ORDER_SIDE_BUY, ORDER_SIDE_SELL, \
+        ORDER_TYPE_MARKET, ORDER_TYPE_STOP, ORDER_TYPE_TRAILING_STOP, ORDER_TYPE_TAKE_PROFIT, \
+        ORDER_STATUS_OPEN, ORDER_STATUS_PENDING, ORDER_STATUS_FILLED, ORDER_STATUS_UNTRIGGERED, \
         POSITION_STATUS_OPEN, POSITION_STATUS_CLOSED, \
         MARKET_BTC_USD, MARKET_ETH_USD, MARKET_AVAX_USD, MARKET_ADA_USD, MARKET_ALGO_USD
 from web3 import Web3
 import pandas as pd
 import numpy as np
+from pprint import pprint
 
 
 load_dotenv()
 WEB3_PROVIDER_URL = 'https://mainnet.infura.io/v3/' + os.getenv('INFURA_PROJECT_ID')
-DEVIATION_THRESHOLD = 1 # multiply of standard deviation
-TARGET_MARKET = MARKET_ALGO_USD
+DEVIATION_THRESHOLD = 0.3 # multiply of standard deviation
+TARGET_MARKET = MARKET_ADA_USD
+ORDER_SIZE = '10'
+ORDER_TYPE = ORDER_TYPE_TRAILING_STOP
+TRAILING_PERCENT = 1
 
 
 client = Client(
     network_id=NETWORK_ID_MAINNET,
     host=API_HOST_MAINNET,
     eth_private_key=os.getenv('ETH_PRIVATE_KEY'),
-    web3=Web3(Web3.HTTPProvider(WEB3_PROVIDER_URL))
+    web3=Web3(Web3.HTTPProvider(WEB3_PROVIDER_URL)),
+    stark_private_key=os.getenv('STARK_PRIVATE_KEY')
 )
 
 
@@ -44,20 +51,47 @@ class DydxData:
         return z_score
 
 
-def place_order(order_type):
-    print(order_type, " -- Creating an order")
+def place_order(order_params):
+    order_creation_response = client.private.create_order(**order_params)
+    print(order_creation_response)
 
 
 def job():
     dydx_data = DydxData()
     z_score = dydx_data.find_z()
+    order_params = {
+        'position_id': 1,
+        'market': TARGET_MARKET,
+        'order_type': ORDER_TYPE,
+        'post_only': True,
+        'size': ORDER_SIZE,
+        'price': str(dydx_data.df['close'].iloc[0]),
+        'limit_fee': 0.001,
+        'expiration_epoch_seconds': 2419200,
+    }
     if not (z_score > DEVIATION_THRESHOLD or z_score < -DEVIATION_THRESHOLD):
         print("Deviation does not exceed the limit")
         return
     elif z_score > DEVIATION_THRESHOLD:
-        place_order(order_type="BUY")
+        order_params = {
+            **order_params,
+            'side': ORDER_SIDE_BUY,
+            'trailing_percent': TRAILING_PERCENT
+        }
+        place_order(order_params)
         return
     elif z_score < -DEVIATION_THRESHOLD:
-        place_order(order_type="SELL")
+        order_params = {
+            **order_params,
+            'side': ORDER_SIDE_SELL,
+            'trailing_percent': -TRAILING_PERCENT
+        }
+        place_order(order_params)
         return
+
+    orders_response = client.private.get_orders(
+        market=TARGET_MARKET,
+        status=ORDER_STATUS_UNTRIGGERED
+    )
+    pprint(orders_response.data)
 
